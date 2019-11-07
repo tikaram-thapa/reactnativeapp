3b75883b6cbec09a78566668aec58c8c4342078c
azminds03 <tikaramthapamgr@gmail.com>
2019-11-07 17:51:01 +0545

    redux persist add and setup config

--- App.js
+++ App.js
@@ -8,15 +8,19 @@
 
 import React, { Component } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 import Main from "./src/Main";
-import store from "./src/config/Store";
+import persist from "./src/config/Store";
 
+const persistStore = persist();
 export default class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Main />
+      <Provider store={persistStore.store}>
+        <PersistGate loading={null} persistor={persistStore.persistor}>
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
--- src/config/Store.js
+++ src/config/Store.js
@@ -1,7 +1,18 @@
+import { AsyncStorage } from "react-native";
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
 
 import reducers from "../reducers";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default () => {
+  let store = createStore(persistedReducer, {}, applyMiddleware());
+  let persistor = persistStore(store);
+  return { store, persistor };
+};
